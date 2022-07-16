a13a4cde240b640b2f003c5cb830f72f8c94cb87
drunker121 <87312371+drunker121@users.noreply.github.com>
2022-07-16 20:53:15 +0530

    first

---
+++ src/components/News.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import NewsItems from "./NewsItems";
+
+export default class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page : 1
+    }
+  }
+
+  async componentDidMount() {
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=286e5013a26842d69130ccb801d9cdc1&page=1&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles , totalResults: parsedData.totalResults });
+  } 
+
+ handlePrevClick = async ()=>{
+  let url =
+  `https://newsapi.org/v2/top-headlines?country=in&apiKey=286e5013a26842d69130ccb801d9cdc1&page=${this.state.page - 1 }&pageSize=${this.props.pageSize}`;
+let data = await fetch(url);
+let parsedData = await data.json();
+console.log(parsedData);
+
+
+   this.setState({
+     page: this.state.page - 1,
+     articles: parsedData.articles
+   })
+  }
+  handleNextClick = async ()=>{
+    if (this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+
+    }
+    else{
+    let url =
+    `https://newsapi.org/v2/top-headlines?country=in&apiKey=286e5013a26842d69130ccb801d9cdc1&page=${this.state.page + 1 }&pageSize=${this.props.pageSize}`;
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  console.log(parsedData);
+ 
+
+     this.setState({
+       page: this.state.page + 1,
+       articles: parsedData.articles
+     })
+    }
+  }
+
+  render() { 
+    return (
+      <div className="container my-3">
+        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        <div className="row">
+         {this.state.articles.map((element)=>{
+           return <div className="col-md-4" key={element.url}>
+             <NewsItems title={element.title?element.title:""} decription = {element.description?element.description:""} imageUrl = {element.urlToImage} newsUrl= {element.url}/>
+             </div>
+         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled ={this.state.page<=1}   type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+        <button disabled={this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/NewsItems.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export class NewsItems extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://images.hindustantimes.com/tech/img/2022/01/14/1600x900/James_Webb_in_space_1640437238388_1642168020136.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItems;
